4d438f42e3277c0b42bfae307302697d1e0e9b4e
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-07-29 16:45:27 +0100

    Add colours, font sizes and spacing sizes

--- red-fern-theme/tailwind.config.js
+++ red-fern-theme/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       'sans': 'var(--font-family--primary)'
     },
     fontSize: {
+      '2xs': 'var(--font-size--2xs)',
       'xs': 'var(--font-size--xs)',
       'sm': 'var(--font-size--sm)',
       'base': 'var(--font-size--base)',
@@ -18,12 +19,19 @@ module.exports = {
       'lg': 'var(--font-size--lg)',
       'xl': 'var(--font-size--xl)',
       '2xl': 'var(--font-size--2xl)',
-      '3xl': 'var(--font-size--3xl)',
-      '4xl': 'var(--font-size--4xl)',
-      '5xl': 'var(--font-size--5xl)'
+      '3xl': 'var(--font-size--3xl)'
     },
     extend: {
       colors: {
+        'black': 'var(--color-black)',
+        'navy': 'var(--color-navy)',
+        'grey-dark': 'var(--color-grey-dark)',
+        'grey-light': 'var(--color-grey-light)',
+        'grey': 'var(--color-grey)',
+        'teal-dark': 'var(--color-teal-dark)',
+        'teal': 'var(--color-teal)',
+        'off-white': 'var(--color-off-white)',
+        'white': 'var(--color-white)'
       },
       spacing: {
         'xs': 'var(--spacing--xs)',
